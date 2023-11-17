6f49d5a0edc2e4db9bde7783a7c7a531194a1acb
diegoromero1 <70231529+diegoromero1@users.noreply.github.com>
2023-11-16 21:11:50 -0300

    Agregacion solicitud tipo

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
 # Proyecto de Web Scraping - SII
-Este proyecto realiza web scraping en el sitio web del Servicio de Impuestos Internos (SII) de Chile 
+- Este proyecto realiza web scraping en el sitio web del Servicio de Impuestos Internos (SII) de Chile 
 para obtener información sobre el impuesto específico del petróleo. 
-Los datos extraídos se guardan en un archivo CSV.
+- Los datos extraídos se guardan en un archivo CSV.
 
 ## Requisitos
-Python
-Paquetes necesarios (instalados mediante `pip install -r requirements.txt`)
+- Python
+- Paquetes necesarios (instalados mediante `pip install -r requirements.txt`)
+
+## Instalación
+1. Clona este repositorio:
+
+   ```bash
+   git clone https://github.com/diegoromero1/PruebaTecnicaScraper.git 
+   ```
+2. Instala las dependencias:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+   
+## Uso
+- Ejecuta el scraper desde la línea de comandos:
+
+```bash
+python ScraperSII.py
+```
+- EL codigo va a solicitar ingresar un año el cual utilizara para la direccion url
+que permitira modificar el url principal para poder ir buscando y extraer los datos por año.
+- El codigo tambien solicitara el tipo de combustible a buscar mediante un listado de opciones.
+- Como salida se genera una tabla por pantalla y tambien un archivo csv que contendra el año y el
+tipo en su nombre donde todos representa que extrajo toda la informacion.
--- ScraperSII.py
+++ ScraperSII.py
@@ -4,18 +4,52 @@ from prettytable import PrettyTable
 import csv
 from unidecode import unidecode
 
+# Diccionario para mapear el número del tipo de combustible a su nombre
+tipos = {
+    '1': 'Gasolina Automotriz 93 (*)',
+    '2': 'Gasolina Automotriz 97 (*)',
+    '3': 'Petróleo Diesel',
+    '4': 'Gas Licuado de Petróleo de Consumo Vehicular',
+    '5': 'Gas Natural Comprimido de Consumo Vehicular',
+    '6': 'Todos'
+}
 
-# Función para obtener la URL basada en el año proporcionado por el usuario
-def obtener_url():
-    ano = input("Ingrese el año para obtener la información (por ejemplo, 2022, 2023, etc.): ")
-    url = f'https://www.sii.cl/valores_y_fechas/mepco/mepco{ano}.htm'
-    return ano, url
+
+# Función para obtener el año y la URL basada en el año proporcionado por el usuario
+def obtener_ano_url():
+    while True:
+        ano = input("Ingrese el año para obtener la información (por ejemplo, 2022, 2023, etc.): ")
+        if ano.isdigit() and len(ano) == 4:
+            url = f'https://www.sii.cl/valores_y_fechas/mepco/mepco{ano}.htm'
+            return ano, url
+        else:
+            print("Año no valido. Por favor, ingrese un año válido de 4 dígitos.")
+
+
+# Función para obtener el tipo de combustible
+def obtener_tipo_combustible():
+    print("Seleccione el tipo de combustible:")
+    for key, value in tipos.items():
+        print(f"{key}. {value}")
+
+    while True:
+        opcion = input("Ingrese el numero correspondiente al tipo de combustible o '6' para Todos: ")
+        if opcion in tipos:
+            return opcion
+        else:
+            print("Opcion no valida. Por favor, ingrese un numero del 1 al 6.")
 
 
 # Función principal para realizar el scraping y guardar los datos
 def scrape_sii():
-    # Obtener la URL y el año según la entrada del usuario
-    ano, url = obtener_url()
+    # Obtener el año y la URL según la entrada del usuario
+    ano, url = obtener_ano_url()
+
+    # Obtener el tipo de combustible según la entrada del usuario
+    tipo_combustible = obtener_tipo_combustible()
+
+    # Obtener el nombre del tipo de combustible para el archivo CSV
+    nombre_tipo_combustible = tipos[tipo_combustible].lower().replace(' ', '_').replace('(*)', '')
 
     # Realizar la solicitud HTTP
     response = requests.get(url)
@@ -43,7 +77,7 @@ def scrape_sii():
             tabla_resultados = PrettyTable(encabezados)
 
             # Guardar los datos en un archivo CSV
-            with open(f'datos_sii_{ano}.csv', 'w', encoding='utf-8', newline='') as csv_file:
+            with open(f'datos_sii_{nombre_tipo_combustible}_{ano}.csv', 'w', encoding='utf-8', newline='') as csv_file:
                 writer = csv.writer(csv_file)
 
                 # Escribir encabezados en el archivo CSV
@@ -68,50 +102,53 @@ def scrape_sii():
                             # Extraer el valor correspondiente al tipo de combustible desde el td
                             tipo_combustible_valor = tipo_combustible_elemento.text.strip()
 
-                            # Extraer los valores correspondientes al tipo de combustible desde los td,
-                            # ignorando el <td>Impuesto</td>
-                            valores_combustible = [
-                                td.text.strip() for td in fila.find_all('td', {'class': None}) if
-                                td.text.strip() not in ['Impuesto', tipo_combustible_valor]
-                            ]
+                            # Verificar si se seleccionó 'Todos' o si coincide con el tipo de combustible actual
+                            if tipo_combustible == '6' or tipo_combustible_valor.startswith(tipos[tipo_combustible]):
+                                # Extraer los valores correspondientes al tipo de combustible desde los td,
+                                # ignorando el <td>Impuesto</td>
+                                valores_combustible = [
+                                    td.text.strip() for td in fila.find_all('td', {'class': None}) if
+                                    td.text.strip() not in ['Impuesto', tipo_combustible_valor]
+                                ]
 
-                            # Asegurarse de que la cantidad de valores coincida con la cantidad de encabezados
-                            if len(valores_combustible) + 2 == len(encabezados):  # +2 por 'Vigencia' y 'Tipo'
-                                # Agregar fila a la tabla PrettyTable
-                                tabla_resultados.add_row([vigencia, tipo_combustible_valor] + valores_combustible)
+                                # Asegurarse de que la cantidad de valores coincida con la cantidad de encabezados
+                                if len(valores_combustible) + 2 == len(encabezados):  # +2 por 'Vigencia' y 'Tipo'
+                                    # Agregar fila a la tabla PrettyTable
+                                    tabla_resultados.add_row([vigencia, tipo_combustible_valor] + valores_combustible)
 
-                                # Escribir fila en el archivo CSV
-                                writer.writerow(
-                                    [unidecode(vigencia), unidecode(tipo_combustible_valor)] + [unidecode(valor) for
-                                                                                                valor in
-                                                                                                valores_combustible])
-                            elif tipo_combustible_valor == 'Otro' and len(valores_combustible) == len(encabezados) - 2:
-                                # Manejar el caso específico de "Otro" sin 'Tipo' en los valores
-                                # Agregar fila a la tabla PrettyTable
-                                tabla_resultados.add_row(
-                                    [vigencia, tipo_combustible_valor] + valores_combustible + [''])
+                                    # Escribir fila en el archivo CSV
+                                    writer.writerow(
+                                        [unidecode(vigencia), unidecode(tipo_combustible_valor)] + [unidecode(valor) for
+                                                                                                    valor in
+                                                                                                    valores_combustible])
+                                elif tipo_combustible_valor == 'Otro' and len(
+                                        valores_combustible) == len(encabezados) - 2:
+                                    # Manejar el caso específico de "Otro" sin 'Tipo' en los valores
+                                    # Agregar fila a la tabla PrettyTable
+                                    tabla_resultados.add_row(
+                                        [vigencia, tipo_combustible_valor] + valores_combustible + [''])
 
-                                # Escribir fila en el archivo CSV
-                                writer.writerow(
-                                    [unidecode(vigencia), unidecode(tipo_combustible_valor)] + [unidecode(valor) for
-                                                                                                valor in
-                                                                                                valores_combustible] + [
-                                        ''])
-                            else:
-                                print(
-                                    f"La cantidad de valores no coincide con la cantidad de encabezados para {tipo_combustible_valor}.")
-                                print("Encabezados:", encabezados)
-                                print("Valores:", [vigencia, tipo_combustible_valor] + valores_combustible)
-                                return
+                                    # Escribir fila en el archivo CSV
+                                    writer.writerow(
+                                        [unidecode(vigencia), unidecode(tipo_combustible_valor)] + [unidecode(valor) for
+                                                                                                    valor in
+                                                                                                    valores_combustible] + [
+                                            ''])
+                                else:
+                                    print(
+                                        f"La cantidad de valores no coincide con la cantidad de encabezados para {tipo_combustible_valor}.")
+                                    print("Encabezados:", encabezados)
+                                    print("Valores:", [vigencia, tipo_combustible_valor] + valores_combustible)
+                                    return
 
             # Imprimir la tabla
             print(tabla_resultados)
 
-            print(f"Datos extraídos y guardados exitosamente en 'datos_sii_{ano}.csv'.")
+            print(f"Datos extraidos y guardados exitosamente en 'datos_sii_{nombre_tipo_combustible}_{ano}.csv'.")
         else:
-            print("No se encontraron tablas en la página con la clase 'table table-bordered'.")
+            print("No se encontraron tablas en la pagina con la clase 'table table-bordered'.")
     else:
-        print(f"Error al acceder al sitio web. Código de estado: {response.status_code}")
+        print(f"Error al acceder al sitio web. Codigo de estado: {response.status_code}")
 
 
 # Entrada principal del programa
